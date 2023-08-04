51d1632dc00dcd04d42966cd7375b1d7d3dda69e
Romain Candusso <r.candusso@protonmail.com>
2023-08-04 12:16:56 +0200

    feat: table of daily dashboard

--- src/pages/operationalDashboard/TableComp.vue
+++ src/pages/operationalDashboard/TableComp.vue
@@ -1,20 +1,12 @@
 <template>
-  <a-table :columns="columns" :data-source="data" @change="handleTableChange">
-    <template #bodyCell="{ column, record }">
-      <template v-if="column.key === 'statut'">
-        <span>
-          <a-tag
-            v-for="statut in record.statut"
-            :key="statut"
-            :color="statut === 'libre' ? 'green' : 'volcano'"
-          >
-            {{ statut.toUpperCase() }}
-          </a-tag>
-        </span>
-      </template>
-    </template>
+  <a-table :columns="columns" :data-source="data">
 
     <template #headerCell="{ column }">
+      <template v-if="column.key === 'tags'">
+        <span>
+          Statut
+        </span>
+      </template>
       <template v-if="column.key === 'chambre'">
         <span>
           Chambre
@@ -37,6 +29,18 @@
       </template>
     </template>
 
+    <template #bodyCell="{ column, record }">
+      <template v-if="column.key === 'tags'">
+        <span>
+          <a-tag :key="record.tags"
+            :color="record.tags === 'disponible' ? 'green' : 'volcano'"
+          >
+            {{ record.tags.toUpperCase() }}
+          </a-tag>
+        </span>
+      </template>
+    </template>
+
     
   </a-table>
 </template>
@@ -46,11 +50,23 @@
 export default {
   data() {
     return {
-      columns : [{
+      columns : [
+      {
         name: 'Statut',
-        dataIndex: 'statut',
-        key: 'statut'
-      }, {
+        dataIndex: 'tags',
+        key: 'tags',
+        filters: [{
+          text: 'Disponible',
+          value: 'disponible'
+        },
+        {
+          text: 'Occupée',
+          value: 'occupée'
+        }],
+        onFilter: (value, record) => record.tags === value,
+        filterSearch: true
+      },
+      {
         title: 'Chambre',
         dataIndex: 'chambre',
         key: 'chambre',
@@ -76,21 +92,17 @@ export default {
         key: 'nombreCle',
       }],
       data : [{
-        statut: ['libre'],
+        tags: 'disponible',
         chambre: 32,
-        nom: 'Jean-Jacques Goldman',
-        arrivee: '08/09/2023',
-        depart: '10/09/2023',
-        nombreCle: 2
       }, {
-        statut: ['occupée'],
+        tags: 'occupée',
         chambre: 42,
         nom: 'Gean-Jacques Goldman',
         arrivee: '08/09/2023',
         depart: '11/09/2023',
         nombreCle: 2
       }, {
-        statut: ['libre'],
+        tags: 'occupée',
         chambre: 32,
         nom: 'Jean-Jacques Goldman',
         arrivee: '08/09/2023',
