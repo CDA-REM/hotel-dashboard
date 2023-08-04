fe36a61528f4dc698bd44a5d57f412f2878503e1
Emilie Ickart <emi.ickart@gmail.com>
2023-08-04 12:14:28 +0200

    Add RangePicker.vue component

---
+++ src/components/RangePicker.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import type { Dayjs } from 'dayjs';
+
+type RangeValue = [Dayjs, Dayjs];
+const value1 = ref<RangeValue>();
+const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY'];
+</script>
+
+<template>
+  <a-space direction="vertical" :size="12">
+    <a-range-picker v-model:value="value1" :format="dateFormatList" :placeholder="['Du','Au']" />
+  </a-space>
+</template>
+
+<style scoped>
+
+</style>
